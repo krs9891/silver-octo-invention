b3c1451590b100099fa4e1484ba6060962734bdc
krs9891 <krzysztof.russek@protonmail.com>
2024-12-08 09:28:32 +0100

    Added CurrencyPairFilter to admin for filtering currency pairs and improved XLSX export functionality with rounded exchange rates

--- api/admin.py
+++ api/admin.py
@@ -3,9 +3,29 @@ from api.models import Currency, CurrencyExchangeRate
 from django.http import HttpResponse
 import openpyxl
 
+class CurrencyPairFilter(admin.SimpleListFilter):
+    title = 'currency pair'
+    parameter_name = 'currency_pair'
+
+    def lookups(self, request, model_admin):
+        pairs = set(
+            CurrencyExchangeRate.objects.values_list(
+                'base_currency__code', 'quote_currency__code'
+            )
+        )
+        return [(f"{base}{quote}", f"{base}{quote}") for base, quote in pairs]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            base, quote = self.value()[:3], self.value()[3:]
+            return queryset.filter(base_currency__code=base, quote_currency__code=quote)
+        return queryset
+
 class CurrencyExchangeRateAdmin(admin.ModelAdmin):
     list_display = ['currency_pair', 'exchange_rate', 'date']
+    list_filter = [CurrencyPairFilter, 'date']
     actions = ['export_to_xlsx']
+    ordering = ['-date']
 
     def export_to_xlsx(self, request, queryset):
         workbook = openpyxl.Workbook()
@@ -14,9 +34,9 @@ class CurrencyExchangeRateAdmin(admin.ModelAdmin):
         sheet.append(["Currency Pair", "Exchange Rate", "Date"])
         
         for rate in queryset:
-            sheet.append([rate.currency_pair(), rate.exchange_rate, rate.date])
+            sheet.append([rate.currency_pair(), round(rate.exchange_rate, 3), rate.date])
 
-        response = HttpResponse(content_type="application/vnd.ms-excel")
+        response = HttpResponse(content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
         response['Content-Disposition'] = 'attachment; filename="currency_rates.xlsx"'
         workbook.save(response)
         return response
